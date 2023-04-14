acbd1aacef0e6b3b53872d967f20bd0e82c695cf
Sean Martin <sean@seanmartinstudio.com>
2023-04-13 21:52:46 -0700

    adds vertical fader bar

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 let col = 0;
+let b = 0;
+let r = 0;
 
 
 function setup() {
@@ -7,16 +9,32 @@ function setup() {
 }
 
 function draw() {
-  background(col);
+  background(r, 0, b);
+
+  b = map(mouseX, 0, windowWidth, 0, 255);
+  r = map(mouseX, 0, windowWidth, 0, 255);
 
   //maps color range from 0 -> 255 with mouseX position
   col = map(mouseX, 0, windowWidth, 0, 255);
 
-  //create line across x axis that starts and stops shortly before edges of window width
+  //horizontal slider
   line(0, windowHeight/2, windowWidth, windowHeight/2)
   strokeWeight(20)
 
-  //create circle the the same y axis as line
-  circle(mouseX, windowHeight/2, 20)
+   //vertical slider
+  line(mouseX, 0, mouseX, windowHeight)
+  strokeWeight(20)
+
+  //horizontal circle
+  circle(mouseX, windowHeight/2, 30)
+  fill(0)
+  //vertical circle
+  circle(mouseX, mouseY, 30)
+  fill(0)
+
+
+
+  // //vertical slider
+  // line(mouseX, 0, mouseX, windowHeight)
   //create mouse press function that allows circle to move the same path as line with a limit to each edge of line
 }
